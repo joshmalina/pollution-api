d3f0fd5d672703df8e1dcacfbadbfe1c4abeabbb
Joshua Malina <joshuamalina@g.harvard.edu>
2015-10-20 10:16:08 -0500

    updates

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
-from eve import Eve
+#from eve import Eve
 import forecast
 import rforest
-import flask
+from flask import Flask, json
 from decorators import crossdomain
-app = Eve()
+#app = Eve()
+app = Flask(__name__)
+
 
 @app.route('/forecast')
 @crossdomain(origin='http://localhost:9000')
@@ -11,7 +13,7 @@ def index():
 	forest = rforest.forest(train=False)
 	frcast = forecast.forecast(forest.X_train.columns, forest.rf)
 	#return flask.json.jsonify({'predictions':frcast.df})
-	return flask.json.jsonify(frcast.df)
+	return json.jsonify(frcast.df)
 
 if __name__ == '__main__':
 	app.run(debug=True)
